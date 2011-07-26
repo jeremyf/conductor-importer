b04005253e1d5693f9b54d2f4c503e5776ad9e4e
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2011-07-26 19:30:41 -0400

    Update Conductor::Importer process to fragement behavior.

--- lib/conductor-importer/batch.rb
+++ lib/conductor-importer/batch.rb
@@ -1,7 +1,10 @@
 require 'state_machine'
 module Conductor
   module Importer
+    # Each of the states has these methods; However when the batch is in the
+    # given state, the method is overridden.
     module BatchCommands
+
       def download(*args); end
       def process_images(*args); end
       def process_links(*args); end
@@ -10,10 +13,10 @@ module Conductor
 
     end
     class Batch < ActiveRecord::Base
-      has_many :pages, :dependent => :destroy
-      has_many :referenced_objects, :dependent => :destroy
-      has_many :images
-      has_many :links
+      has_many :pages, :dependent => :destroy, :class_name => '::Conductor::Importer::Page'
+      has_many :referenced_objects, :dependent => :destroy, :class_name => '::Conductor::Importer::ReferencedObject'
+      has_many :images, :class_name => '::Conductor::Importer::Image'
+      has_many :links, :class_name => '::Conductor::Importer::Link'
 
 
       state_machine :state, :initial => :started do
@@ -24,33 +27,7 @@ module Conductor
           include BatchCommands
           def download(entries)
             entries.each do |entry|
-              self.pages.build(entry).tap do |page|
-                page.batch = self
-                response = RestClient.get(entry['source_url'], :accept => :html)
-                doc = Hpricot(response.body)
-
-                # Collect the parts
-                entry['content_map'].each do |options|
-                  source_content = options['selector_method_chain'].inject(doc) { |mem, var|
-                    mem = mem.send(*var)
-                  }
-
-                  # Scan for images
-                  (source_content/'img').each do |img_tag|
-                    page.referenced_objects << self.images.build(:batch => self, :source_url => img_tag.get_attribute('src'))
-                  end
-
-                  # Scan for images
-                  (source_content/'a').each do |a_tag|
-                    if a_tag.get_attribute('href') !~ /^\#/
-                      page.referenced_objects << self.links.build(:batch => self, :source_url => a_tag.get_attribute('href'))
-                    end
-                  end
-
-                  page.page_attributes.build(:key => options['target_attribute'], :value => source_content.collect(&:to_s).join("\n"))
-                end
-                page.save!
-              end
+              Page.create_from!(self, entry)
             end
             self.download_completed!
           end
@@ -81,24 +58,76 @@ module Conductor
           def upload
           end
         end
+
+        state :uploaded do
+          include BatchCommands
+        end
       end
     end
 
 
     class Page < ActiveRecord::Base
-      belongs_to :batch
-      has_many :page_attributes, :dependent => :destroy
+      belongs_to :batch, :class_name => '::Conductor::Importer::Batch'
+      has_many :page_attributes, :dependent => :destroy, :class_name => '::Conductor::Importer::PageAttribute'
       serialize :content_map
       has_and_belongs_to_many :referenced_objects
 
-      state_machine :state, :initial => :downloaded
+      def self.create_from!(batch, entry)
+        batch.pages.find_or_initialize_by_batch_id_and_source_url(batch[:id], entry['source_url']).process!(batch,entry)
+      end
+
+      def process!(batch,entry)
+        self.batch = batch
+        self.content_map = entry['content_map']
+        self.target_url = entry['target_url']
+        self.name = entry['name']
+
+        response = RestClient.get(entry['source_url'], :accept => :html)
+        doc = Hpricot(response.body)
+
+        # Collect the parts
+        content_map.each do |options|
+          source_content = options['selector_method_chain'].inject(doc) { |mem, var|
+            mem = mem.send(*var)
+          }
+
+          # Scan for images
+          (source_content/'img').each do |img_tag|
+            self.referenced_objects << batch.images.build(:batch => self.batch, :source_url => img_tag.get_attribute('src'))
+          end
+
+
+          # Scan for images
+          (source_content/'a').each do |a_tag|
+            if a_tag.get_attribute('href') !~ /^\#/
+              self.referenced_objects << batch.links.build(:batch => self.batch, :source_url => a_tag.get_attribute('href'))
+            end
+          end
+
+          self.page_attributes.build(:key => options['target_attribute'], :value => source_content.collect(&:to_s).join("\n"))
+        end
+        # self.save!
+        self.download_complete!
+      end
+
+      state_machine :state, :initial => :preprocess do
+        event :download_complete do
+          transition :preprocess => :downloaded
+        end
+
+        state :preprocess
+        state :downloaded
+        state :content_transformed
+        state :uploaded
+      end
+
     end
     class PageAttribute < ActiveRecord::Base
-      belongs_to :page
+      belongs_to :page, :class_name => '::Conductor::Importer::Page'
     end
     class ReferencedObject < ActiveRecord::Base
-      belongs_to :batch
-      has_and_belongs_to_many :pages
+      belongs_to :batch, :class_name => '::Conductor::Importer::Batch'
+      has_and_belongs_to_many :pages, :class_name => '::Conductor::Importer::Page'
     end
     class Image < ReferencedObject
     end
