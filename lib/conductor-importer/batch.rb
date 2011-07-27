80eb13d439d00247ace61ad111b5c6bcf26d1035
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2011-07-26 20:15:37 -0400

    Updated importer to allow Pages to start and stop download.

--- lib/conductor-importer/batch.rb
+++ lib/conductor-importer/batch.rb
@@ -12,6 +12,7 @@ module Conductor
       def upload(*args); end
 
     end
+
     class Batch < ActiveRecord::Base
       has_many :pages, :dependent => :destroy, :class_name => '::Conductor::Importer::Page'
       has_many :referenced_objects, :dependent => :destroy, :class_name => '::Conductor::Importer::ReferencedObject'
@@ -66,6 +67,11 @@ module Conductor
     end
 
 
+    module PageCommands
+      def download!(*args); end
+    end
+
+
     class Page < ActiveRecord::Base
       belongs_to :batch, :class_name => '::Conductor::Importer::Batch'
       has_many :page_attributes, :dependent => :destroy, :class_name => '::Conductor::Importer::PageAttribute'
@@ -73,41 +79,7 @@ module Conductor
       has_and_belongs_to_many :referenced_objects
 
       def self.create_from!(batch, entry)
-        batch.pages.find_or_initialize_by_batch_id_and_source_url(batch[:id], entry['source_url']).process!(batch,entry)
-      end
-
-      def process!(batch,entry)
-        self.batch = batch
-        self.content_map = entry['content_map']
-        self.target_url = entry['target_url']
-        self.name = entry['name']
-
-        response = RestClient.get(entry['source_url'], :accept => :html)
-        doc = Hpricot(response.body)
-
-        # Collect the parts
-        content_map.each do |options|
-          source_content = options['selector_method_chain'].inject(doc) { |mem, var|
-            mem = mem.send(*var)
-          }
-
-          # Scan for images
-          (source_content/'img').each do |img_tag|
-            self.referenced_objects << batch.images.build(:batch => self.batch, :source_url => img_tag.get_attribute('src'))
-          end
-
-
-          # Scan for images
-          (source_content/'a').each do |a_tag|
-            if a_tag.get_attribute('href') !~ /^\#/
-              self.referenced_objects << batch.links.build(:batch => self.batch, :source_url => a_tag.get_attribute('href'))
-            end
-          end
-
-          self.page_attributes.build(:key => options['target_attribute'], :value => source_content.collect(&:to_s).join("\n"))
-        end
-        # self.save!
-        self.download_complete!
+        batch.pages.find_or_initialize_by_batch_id_and_source_url(batch[:id], entry['source_url']).download!(batch,entry)
       end
 
       state_machine :state, :initial => :preprocess do
@@ -115,10 +87,52 @@ module Conductor
           transition :preprocess => :downloaded
         end
 
-        state :preprocess
-        state :downloaded
-        state :content_transformed
-        state :uploaded
+        state :preprocess do
+          include PageCommands
+          def download!(batch,entry)
+
+            self.batch = batch
+            self.content_map = entry['content_map']
+            self.target_url = entry['target_url']
+            self.name = entry['name']
+
+            response = RestClient.get(entry['source_url'], :accept => :html)
+            doc = Hpricot(response.body)
+
+            # Collect the parts
+            content_map.each do |options|
+              source_content = options['selector_method_chain'].inject(doc) { |mem, var|
+                mem = mem.send(*var)
+              }
+
+              # Scan for images
+              (source_content/'img').each do |img_tag|
+                self.referenced_objects << batch.images.build(:batch => self.batch, :source_url => img_tag.get_attribute('src'))
+              end
+
+
+              # Scan for images
+              (source_content/'a').each do |a_tag|
+                if a_tag.get_attribute('href') !~ /^\#/
+                  self.referenced_objects << batch.links.build(:batch => self.batch, :source_url => a_tag.get_attribute('href'))
+                end
+              end
+
+              self.page_attributes.build(:key => options['target_attribute'], :value => source_content.collect(&:to_s).join("\n"))
+            end
+            # self.save!
+            self.download_complete!
+          end
+        end
+        state :downloaded do
+          include PageCommands
+        end
+        state :content_transformed do
+          include PageCommands
+        end
+        state :uploaded do
+          include PageCommands
+        end
       end
 
     end
